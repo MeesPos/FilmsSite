f7b170ca9b2163e59820b7b6072312301d453578
Cornell van der Straaten <30183@ma-web.nl>
2020-07-19 10:40:53 +0200

    htaccess verbeterd

---
+++ private/controllers/RegistrationLoginController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Website\Controllers;
+
+/**
+ * Class RegistrationLoginController
+ *
+ * Deze handelt de logica van de registratie en login pagina's af
+ * Haalt gegevens uit de "model" laag van de website (de gegevens)
+ * Geeft de gegevens aan de "view" laag (HTML template) om weer te geven
+ *
+ */
+class RegistrationLoginController {
+
+	public function registerHome() {
+
+		$template_engine = get_template_engine();
+		echo $template_engine->render('registerPage');
+
+	}
+
+	
+}
+
--- private/routes.php
+++ private/routes.php
@@ -11,8 +11,12 @@ SimpleRouter::group( [ 'prefix' => site_url() ], function () {
 	// START: Zet hier al eigen routes
 	// Lees de docs, daar zie je hoe je routes kunt maken: https://github.com/skipperbent/simple-php-router#routes
 
+	// Homepage
 	SimpleRouter::get( '/', 'WebsiteController@home' )->name( 'home' );
 	SimpleRouter::get( '/api', 'WebsiteController@api' )->name( 'api' );
+
+	// Registration & Login
+	SimpleRouter::get( 'registration', 'RegisterLoginController@registerHome' )->name( 'register-home' );
 
 
 	// STOP: Tot hier al je eigen URL's zetten

89d7d2b0d264b5ff224f8457cf645ba9ad8a4950
Cornell van der Straaten <30183@ma-web.nl>
2020-07-20 10:46:27 +0200

    Start css until card content & further html

--- private/views/registration.php
+++ private/views/registration.php
@@ -14,33 +14,49 @@
 
 <body>
 	<div class="registerHomeButton">
-		<h2 class="red-text">WMNM</h2>
+		<a href="<?php echo url('/') ?>">
+			<h2 class="red-text">WMNM</h2>
+		</a>
 	</div>
-	<div class="registerCard">
-		<h1 class="registerTagline">Sign up for <span class="red-text">what's my next movie</span></h1>
-		<div class="registerCardContent" style="background: url(<?php echo site_url('img/registerImage.png') ?>);">
-			<div class="registerLeft">
-				<form action="<?php echo site_url() ?>" method="POST">
-					<div class="registerInputs">
-						<div class="registerEmailInput">
-							<label for="registerEmail">Email<span class="red-text">*</span></label>
-							<input type="text" name="email" id="registerEmail" class="inputField">
+	<div class="registerCardContainer">
+		<div class="registerCard">
+			<h1 class="registerTagline">SIGN UP FOR <span class="red-text">WHAT'S MY NEXT MOVIE</span></h1>
+			<div class="registerCardBackground" style="background: url(<?php echo site_url('img/registerImage.png') ?>);">
+				<div class="registerCardContent">
+					<div class="registerLeft">
+						<form action="<?php echo site_url() ?>" method="POST">
+							<div class="registerInputs">
+								<div class="registerEmailInput">
+									<label for="registerEmail">Email <span class="red-text">*</span></label>
+									<input type="text" name="email" id="registerEmail" class="inputField">
+								</div>
+								<div class="registerPasswordInput">
+									<label for="registerPassword">Password <span class="red-text">*</span></label>
+									<input type="password" name="password" id="registerPassword" class="inputField">
+								</div>
+								<div class="registerRepeatPasswordInput">
+									<label for="registerRepeatPassword">Repeat Password <span class="red-text">*</span></label>
+									<input type="password" name="repeat-password" id="registerRepeatPassword" class="inputField">
+								</div>
+								<div class="registerUsernameInput">
+									<label for="registerUsername">Username <span class="red-text">*</span></label>
+									<input type="text" name="username" id="registerUsername" class="inputField">
+
+								</div>
+							</div>
+							<div class="registerButtonDiv">
+								<input type="submit" value="REGISTER" id="registerSubmit">
+							</div>
+						</form>
+						<div class="registerSmallText">
+							<h3><i><span class="red-text">*</span> By clicking Register you agree to the <span class="red-text">Terms and Conditions</span> and <span class="red-text">Privacy Statement</span></i></h3>
+							<p>Already have an account? <a>Sign in</a></p>
 						</div>
-						
-						<input type="password" name="password" id="registerPassword" class="inputField">
-						<input type="password" name="repeat-password" id="registerRepeatPassword" class="inputField">
-						<input type="text" name="username" id="registerUsername" class="inputField">
 					</div>
-					<div class="registerButtonDiv">
-						<input type="submit" value="REGISTER" id="registerSubmit">
+					<div class="registerRight">
+						<h2><span class="red-text"><?php echo '2323' ?></span> PEOPLE WERE BEFORE YOU JOIN <span class="red-text">TODAY </span> TO ACCESS</h2>
 					</div>
-				</form>
-				<div class="registerSmallText">
-				<h2><i><span class="red-text">*</span> By clicking Register you agree to the <span class="red-text">Terms and Conditions</span> and <span class="red-text">Privacy Statement</span><i></h2>
 				</div>
-			</div>
-			<div class="registerRight">
-				<h2><span class="red-text"><?php echo '2323' ?></span> PEOPLE WERE BEFORE YOU JOIN <span class="red-text">TODAY </span> TO ACCESS</h2>
 			</div>
 		</div>
 	</div>
